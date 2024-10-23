6a6ab754cedb8833ba2e99773ebe28e43c5ccc4e
andresr0926 <116583625+andresr0926@users.noreply.github.com>
2024-10-23 10:06:12 -0500

    uploading version_control.py file

---
+++ version_control.py
@@ -0,0 +1,56 @@
+# Andres E. Rodriguez
+
+def main():
+    # while loop to show menu options
+    while True:
+        print('Menu\n'
+              '-------------\n'
+              '1. Encode\n'
+              '2. Decode\n'
+              '3. Quit\n\n')
+        option = int(input('Please enter an option: '))
+        # option to encode password
+        if option == 1:
+            x = encode()
+        # option to decode password
+        elif option == 2:
+            pass
+            # decode(x)
+        #option to quit
+        elif option == 3:
+            break
+        # error handling if they do not choose and option 1 - 3
+        else:
+            print('Please enter and option 1 - 3!')
+
+def encode():
+    # create a list with numbers 0-9
+    num_list = list(range(10))
+    # convert number list to strings
+    num_list = [str(num) for num in num_list]
+    # while loop to get the password to encode
+    while True:
+        curr_pass = input('Please enter your password to encode: ')
+        curr_pass = [digit for digit in curr_pass]
+        x = 0
+        # for loop to verify the inputted password is all numbers 0-9
+        for item in curr_pass:
+            if item not in num_list:
+                print('Password not valid')
+                x = 1
+                break
+        if x == 1:
+            continue
+        # encode password by adding 3 to each digit in the password
+        else:
+            new_pass = [str(int(num)+ 3) for num in curr_pass]
+            final_pass = ''.join(new_pass)
+            print('Your password has been encoded and stored!')
+            return final_pass
+
+def decode(final_pass):
+    pass
+
+
+if __name__ == '__main__':
+    main()
